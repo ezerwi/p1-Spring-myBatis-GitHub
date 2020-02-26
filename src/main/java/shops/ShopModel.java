da5bdab75e7e94bed34ca4552b4edca25a3dcb62
ezerwi <johnwi@knou.ac.kr>
2020-02-26 05:00:22 +0000

    test create Class

---
+++ src/main/java/shops/ShopModel.java
@@ -0,0 +1,17 @@
+package shops;
+
+public class ShopModel {
+    
+    public ShopModel() {
+        
+    }
+    
+    private String shop_num;
+    private String shop_name;
+    private String shop_address;
+    
+    private void set_shop_num(String shop_num) {
+        this.shop_num = shop_num;
+    }
+    
+}
